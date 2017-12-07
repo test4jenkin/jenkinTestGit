b5a6716bc0050add69a8304c9818f63b290d8864
Amit Hada <amit.hada@metacube.com>
2017-12-07 16:38:23 +0530

    smokefiles_test

---
+++ SimpleJenkin.java
@@ -0,0 +1,7 @@
+class SimpleJenkin{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+	 
+	 System.out.println("Commit-1:Hello Java");  
+    }  
+}  
---
+++ javatest.java
@@ -0,0 +1,5 @@
+class javatest{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
